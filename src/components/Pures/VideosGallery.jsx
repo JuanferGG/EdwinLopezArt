fd71bcacfefa1c4356d88afd1fb99c7f6f097f68
unknown <juanfe15777@gmail.com>
2024-04-07 08:49:55 -0500

    version 1.2.1 Edwinlopez.Art - Arreglo Video

--- src/components/Pures/VideosGallery.jsx
+++ src/components/Pures/VideosGallery.jsx
@@ -3,15 +3,18 @@ import { Player } from 'video-react'
 
 const VideosGallery = () => {
     return (
-        <div className='d-flex gap-4 w-100 flex-wrap justify-content-center px-xl-3'>
-            <div style={{ width: '420px' }}>
-                {/* <Player
-                    poster='https://i.imgur.com/wUJ5cEK.jpg'
-                >
-                    <source src='vidEdwin.mp4' />
-                </Player> */}
+        <>
+            <link rel="stylesheet" href="https://video-react.github.io/assets/video-react.css" />
+            <div className='d-flex gap-4 w-100 flex-wrap justify-content-center px-xl-3'>
+                <div style={{ width: '420px', boxShadow: '0 0 10px grey' }}>
+                    <Player
+                        poster='https://i.imgur.com/wUJ5cEK.jpg'
+                    >
+                        <source src='vidEdwin.mp4' />
+                    </Player>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
