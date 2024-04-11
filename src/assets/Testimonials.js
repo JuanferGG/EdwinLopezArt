aebd227453d011ce28bcfea0e6e5621a5b9bab41
unknown <juanfe15777@gmail.com>
2024-04-11 11:19:15 -0500

    version 1.2.1 Edwinlopez.Art - Arreglo testimonios personas

--- src/assets/Testimonials.js
+++ src/assets/Testimonials.js
@@ -1,66 +1,37 @@
-export const testimonials =
-    [
-        {
-            name: "María González",
-            company: "Alcaldia",
-            img_src: "https://i.ibb.co/84h8svL/eight.png",
-            review:
-                "Trabajar con Edwin López Fajardo ha sido una experiencia verdaderamente enriquecedora. Su creatividad y dedicación se reflejan en cada obra de arte que produce. Me siento afortunada de haber colaborado con él en mi proyecto de diseño de interiores; su visión única ha transformado por completo mi espacio.",
-        },
-        {
-            name: "Evelyn",
-            company: "Netflix",
-            img_src: "https://i.ibb.co/k8Jnx61/five.png",
-            review:
-                "Lorem 2 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
-        },
-        {
-            name: "Jack",
-            company: "Google",
-            img_src: "https://i.ibb.co/Yj8pMF8/four.png",
-            review:
-                "Lorem12 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
-        },
-        {
-            name: "Sam",
-            company: "Microsoft",
-            img_src: "https://i.ibb.co/ph360c6/nine.png",
-            review:
-                "Lorem 4 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
-        },
-        {
-            name: "María González",
-            company: "Alcaldía",
-            img_src: "https://i.ibb.co/gwNmrLn/one.png",
-            review:
-                "Trabajar con Edwin López Fajardo ha sido una experiencia verdaderamente enriquecedora. Su creatividad y dedicación se reflejan en cada obra de arte que produce. Me siento afortunada de haber colaborado con él en mi proyecto de diseño de interiores; su visión única ha transformado por completo mi espacio.",
-        },
-        {
-            name: "Laura Martínez",
-            company: "Fotografo Independiente",
-            img_src: "https://i.ibb.co/ph360c6/nine.png",
-            review:
-                "Como amante del arte, siempre he admirado el trabajo de Edwin López Fajardo. Tuve el privilegio de adquirir una de sus pinturas y estoy absolutamente enamorada de ella. Su habilidad para transmitir emociones a través de sus obras es incomparable. ¡Definitivamente recomendaría su trabajo a cualquiera que busque piezas únicas y conmovedoras!",
-        },
-        {
-            name: "Bruno",
-            company: "Netflix",
-            img_src: "https://i.ibb.co/7G9rtfD/six.png",
-            review:
-                "Como fotógrafo profesional, siempre estoy buscando colaborar con artistas que compartan mi pasión por la creatividad y la innovación. Edwin es uno de esos artistas excepcionales que logra capturar la esencia de cada proyecto en sus obras. Trabajar con él ha sido una experiencia inspiradora que ha elevado mi trabajo a nuevos niveles.",
-        },
-        {
-            name: "Vernoica",
-            company: "Facebook",
-            img_src: "https://i.ibb.co/pXMvXhK/three.png",
-            review:
-                "Lorem 8 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
-        },
-        {
-            name: "Astro",
-            company: "Netflix",
-            img_src: "https://i.ibb.co/WzJD5sj/two.png",
-            review:
-                "Lorem 9 ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
-        },
-    ]
+export const testimonials = [
+    {
+        name: "María González",
+        company: "Alcaldía",
+        img_src: "https://i.ibb.co/84h8svL/eight.png",
+        review:
+            "Trabajar con Edwin López Fajardo ha sido una experiencia verdaderamente enriquecedora. Su creatividad y dedicación se reflejan en cada obra de arte que produce. Me siento afortunada de haber colaborado con él en mi proyecto de diseño de interiores; su visión única ha transformado por completo mi espacio.",
+    },
+    {
+        name: "Laura Martínez",
+        company: "Fotógrafo Independiente",
+        img_src: "https://i.ibb.co/ph360c6/nine.png",
+        review:
+            "Como amante del arte, siempre he admirado el trabajo de Edwin López Fajardo. Tuve el privilegio de adquirir una de sus pinturas y estoy absolutamente enamorada de ella. Su habilidad para transmitir emociones a través de sus obras es incomparable. ¡Definitivamente recomendaría su trabajo a cualquiera que busque piezas únicas y conmovedoras!",
+    },
+    {
+        name: "Bruno",
+        company: "Netflix",
+        img_src: "https://i.ibb.co/7G9rtfD/six.png",
+        review:
+            "Trabajar con Edwin fue una experiencia increíble. Su creatividad y talento son evidentes en cada una de sus obras. Me siento agradecido por haber tenido la oportunidad de colaborar con él en un proyecto reciente. ¡Definitivamente espero trabajar juntos nuevamente en el futuro!",
+    },
+    {
+        name: "Vernoica",
+        company: "Facebook",
+        img_src: "https://i.ibb.co/pXMvXhK/three.png",
+        review:
+            "Recientemente tuve el placer de trabajar con Edwin y quedé impresionado por su profesionalismo y creatividad. Su capacidad para transformar ideas en obras de arte es realmente admirable. ¡Recomendaría su trabajo a cualquiera que busque resultados excepcionales!",
+    },
+    {
+        name: "Astro",
+        company: "Netflix",
+        img_src: "https://i.ibb.co/WzJD5sj/two.png",
+        review:
+            "Trabajar con Edwin López Fajardo fue una experiencia maravillosa. Su atención al detalle y su habilidad para capturar la esencia de cada proyecto son extraordinarias. ¡Estoy muy contento con el resultado final y espero trabajar juntos nuevamente en el futuro!",
+    },
+];
